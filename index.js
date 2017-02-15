027c03cd626aa47608e04b479187898c465060b5
U-Zyn Chua <chua@uzyn.com>
2017-02-15 12:34:21 +0800

    Merged from master

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
 require('dotenv').config();
-const request = require("request");
 const honestbee = require('./lib/honestbee');
 
-// 1 = 2 x (5 pax paper), 2 = 5 x cafedirect capsules 
-honestbee.order(2).then(function(response){
+// 1 = 2 x (5 pax paper), 2 = 5 x cafedirect capsules
+
+honestbee.order(2).then((response) => {
   console.log(response);
-  return honestbee.status(response.orderGuid) 
-}).then( (response) => {
-  console.log(response); //response is the same object as the one returned from honestbee.order()
-})
+  return honestbee.status(response.orderGuid);
+}).then((response) => {
+  console.log(response); // response is the same object as the one returned from honestbee.order()
+});
 
